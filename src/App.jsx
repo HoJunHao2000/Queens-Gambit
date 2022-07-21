73b127bef6adf92a52a66bc9b48abd3384fb1a1a
Ho Jun Hao <kennethhojunhao@gmail.com>
2022-07-21 22:17:53 +0800

    added AI

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router";
 import Game from "./chess_client/game/Game";
 import MainPage from "./chess_client/main/MainPage";
 import { GameProvider } from "./chess_client/context/GameContext";
-// import Ai from "./chess_AI/ai";
+import AI from "./chess_AI/AI";
 
 export default function App() {
   return (
@@ -13,7 +13,7 @@ export default function App() {
         <Routes>
           <Route path="/" exact element={<MainPage />} />
           <Route path="/game" element={<Game />} />
-          {/* <Route path="/AI" exact element={<Ai />} /> */}
+          <Route path="/AI" exact element={<AI />} />
         </Routes>
       </GameProvider>
     </BrowserRouter>
